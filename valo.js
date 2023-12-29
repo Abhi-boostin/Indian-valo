da5ec40450011d28138ed9c00cd01a2c9224279b
Abhimanyu Singh <abhimanyusinghworks@gmail.com>
2023-12-29 20:07:55 +0530

    first commit

---
+++ valo.js
@@ -0,0 +1,59 @@
+const breach = document.querySelector('.breach');
+breach.addEventListener('click', function(){
+    const breachSound = new Audio('voicelines/breach.mp3');
+    breachSound.play();
+});
+
+const chamber = document.querySelector('.chamber');
+chamber.addEventListener('click', function(){
+    const chamberSound = new Audio('voicelines/chamber.mp3');
+    chamberSound.play();
+});
+
+const omen = document.querySelector('.omen');
+omen.addEventListener('click', function(){
+    const omenSound = new Audio('voicelines/omen.mp3');
+    omenSound.play();
+});
+
+const brimstone = document.querySelector('.brimstone');
+brimstone.addEventListener('click', function(){
+    const brimstoneSound = new Audio('voicelines/brimstone.mp3');
+    brimstoneSound.play();
+});
+
+const cypher = document.querySelector('.cypher');
+cypher.addEventListener('click', function(){
+    const cypherSound = new Audio('voicelines/cypher.mp3');
+    cypherSound.play();
+});
+
+const harbor = document.querySelector('.harbor');
+harbor.addEventListener('click', function(){
+    const harborSound = new Audio('voicelines/harbor.mp3');
+    harborSound.play();
+});
+
+const reyna = document.querySelector('.reyna');
+reyna.addEventListener('click', function(){
+    const reynaSound = new Audio('voicelines/reyna.mp3');
+    reynaSound.play();
+});
+
+const sage = document.querySelector('.sage');
+sage.addEventListener('click', function(){
+    const sageSound = new Audio('voicelines/sage.mp3');
+    sageSound.play();
+});
+
+const gekko = document.querySelector('.gekko');
+gekko.addEventListener('click', function(){
+    const gekkoSound = new Audio('voicelines/gekko.mp3');
+    gekkoSound.play();
+});
+
+const phoenix = document.querySelector('.phoenix');
+phoenix.addEventListener('click', function(){
+    const phoenixSound = new Audio('voicelines/phoenix.mp3');
+    phoenixSound.play();
+});
